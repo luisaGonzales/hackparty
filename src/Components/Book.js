7091751c87ddf27a016eced4cb547fda63496402
unknown <alejandraadriantejda@gmail.com>
2017-12-20 17:13:27 -0500

    estilos

--- src/Components/Book.js
+++ src/Components/Book.js
@@ -1,10 +1,40 @@
 import React, { Component } from 'react';
+import {Col, Row} from 'react-bootstrap'
 
-export const Book = ({title, price}) => {
+export const Book = ({}) => {
+    const mybook = {
+        name: 'My book',
+        img: 'http://is2.mzstatic.com/image/thumb/Publication18/v4/db/3a/18/db3a1818-f034-a7fd-85be-170bf8707143/source/100x100bb.jpg',
+        price: 9.99,
+        author: 'invent',
+        link: 'http://www.facebook.com'
+    }
     return (
-        <div>
-            {title}
-            {price}
-        </div>
+        <Row>
+            <Col mdOffset={2} md={4}>
+                <Col md={4}>
+                    <img className='img-book' src={mybook.img}/>
+                </Col> 
+                <Col md={8}>
+                    <p className='text--book'><b>Nombre: </b>{mybook.name}</p>
+                    <p className='text--book'><b>Precio: </b>{mybook.price}</p>
+                    <p className='text--book'><b>Autor: </b>{mybook.author}</p>
+                    <a className='btn btn-book' href={mybook.name}>Compralo Aquí</a>
+                </Col>
+            </Col>
+            <Col md={4}>
+                <Col md={4}>
+                    <img className='img-book' src={mybook.img}/>
+                </Col> 
+                <Col md={8}>
+                    <p className='text--book'><b>Nombre: </b>{mybook.name}</p>
+                    <p className='text--book'><b>Precio: </b>{mybook.price}</p>
+                    <p className='text--book'><b>Autor: </b>{mybook.author}</p>
+                    <a className='btn btn-book' href={mybook.name}>Compralo Aquí</a>
+                </Col>
+            
+            </Col>
+             
+        </Row>
     )
 }
--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,29 +1,29 @@
 import React, { Component } from 'react';
-import {saveIsbn} from '../Actions/Actions';
+// import {saveIsbn} from '../Actions/Actions';
 import {Form, FormGroup, Col, Button, FormControl} from 'react-bootstrap';
 
 export const Search = () => {
     const onSubmit = (e) => {
         e.preventDefault();
-        if(this.isbnSearch.value) {
-            console.log(this.isbnSearch.value);
-            saveIsbn(this.isbnSearch.value);
-        }
+        // if(this.isbnSearch.value) {
+        //     console.log(this.isbnSearch.value);
+        //     saveIsbn(this.isbnSearch.value);
+        // }
     } 
     return (
         <Form horizontal onSubmit={onSubmit}>
+
             <FormGroup controlId="formHorizontalPassword">
-                <Col smOffset={4} sm={4}>
+                <Col mdOffset={4} md={4}>
                     <FormControl type="text" placeholder="Text ISBN" autoComplete="none" inputRef={ref => { this.isbnSearch = ref }}/>
                 </Col>
-            </FormGroup>
-            <FormGroup>
-                <Col smOffset={4} sm={4}>
+                <Col md={4}>
                     <Button type="submit" className="btnSubmit">
                         SearchItunes
                     </Button>
                 </Col>
             </FormGroup>
+            <br/><br/>
         </Form>
     );
 }

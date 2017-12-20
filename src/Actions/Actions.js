a1517561a3dbe08ee0b218908f7fbdabbca4adb8
luisaGonzales <luisagc711@gmail.com>
2017-12-20 12:17:41 -0500

    price

--- src/Actions/Actions.js
+++ src/Actions/Actions.js
@@ -6,6 +6,14 @@ export const searchItunes = () => {
     fetch(url)
     .then(res => res.json())
     .then((out) => {
+        store.setState({
+            price : out.results[0].price
+        });
         console.log('Checkout this JSON! ', out);
+        console.log('price', store.getState().price);
     });
+    if(store.getState().price != null ){
+        console.log('price2', store.getState().price);
+    }
+    
 }
--- src/Store/Store.js
+++ src/Store/Store.js
@@ -2,7 +2,8 @@ import createStore from 'redux-zero';
 
 const initialState = {
     title : "Hackparty", 
-    isbn : 9781786812872
+    isbn : 9781786812872, 
+    itunes: null,
 }
 
 const store = createStore(initialState);
